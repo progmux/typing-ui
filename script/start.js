2ea73c0dd94da42986695f35f0a0806e836582f9
Richard Snider <richardnsnider@gmail.com>
2017-08-22 16:46:01 -0700

    Add webpack watcher to start script

--- script/start.js
+++ script/start.js
@@ -1,6 +1,26 @@
 require(`dotenv`).config();
 const server = require(`./resources/server.js`);
 const builder = require(`./resources/builder.js`);
+const webpackCompiler = require(`./resources/webpackCompiler.js`);
 
 builder.build();
-server.start();
+if (process.env.NODE_ENV === `dev`) {
+    watch();
+} else {
+    server.start();
+}
+
+function watch() {
+    webpackCompiler.watch({
+        ignored: [/node_modules/, /dist/],
+        poll: 1000
+    }, (err, stats) => {
+        console.log(`Changes detected.`);
+        if (server.instance) {
+            console.log(`Closing server.`);
+            server.instance.close();
+        }
+        console.log(`Starting server.`);
+        server.start();
+    });
+}
