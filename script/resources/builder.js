7e24043fa8fff34d62bad93deadc2868c4b610b2
Richard Snider <richardnsnider@gmail.com>
2017-08-22 16:51:08 -0700

    Create dist directory if it does not exist

--- script/resources/builder.js
+++ script/resources/builder.js
@@ -2,6 +2,10 @@ const fs = require(`fs`);
 
 const builder = {
     build: function () {
+        if (!fs.existsSync(`./dist`)){
+            fs.mkdirSync(`./dist`);
+        }
+
         fs.createReadStream(`./src/index.html`)
             .pipe(fs.createWriteStream(`./dist/index.html`));
         fs.createReadStream(`./src/stylesheet.css`)
