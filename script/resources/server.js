00eb46573fc072db5b38a62ec3bcaaf909f5ab72
Richard Snider <richardnsnider@gmail.com>
2017-08-22 12:32:28 -0700

    Remove server reference to dotenv
    dotenv.config is called in start script

--- script/resources/server.js
+++ script/resources/server.js
@@ -1,7 +1,6 @@
 var connect = require(`connect`);
 var serveStatic = require(`serve-static`);
 var path = require(`path`);
-var dotenv = require(`dotenv`);
 
 module.exports = {
   start: function () {
