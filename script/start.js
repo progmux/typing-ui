dd2ef064d98eb3cbd631495c9b98ddda79ffdc58
Richard Snider <richardnsnider@gmail.com>
2017-08-22 16:45:43 -0700

    Fix missing semi colon in start script require

--- script/start.js
+++ script/start.js
@@ -1,6 +1,6 @@
 require(`dotenv`).config();
 const server = require(`./resources/server.js`);
-const builder = require(`./resources/builder.js`)
+const builder = require(`./resources/builder.js`);
 
 builder.build();
 server.start();
