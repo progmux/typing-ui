3c0b3b6fda140bc918ecb89fe69dc17a14f8e404
Richard Snider <richardnsnider@gmail.com>
2017-08-29 07:24:20 -0700

    Define stats type in js doc

--- src/home.js
+++ src/home.js
@@ -14,6 +14,7 @@ export default {
      */
 
     init: function () {
+        /** @type {TypingStats} */
         let stats = {
             startTime: moment().unix(),
             wordlist: getWordList(),
